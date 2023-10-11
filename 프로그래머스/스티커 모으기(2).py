893efb8c47dcdbcb2e32f489edf26caa19ccadd9
js3617 <a332210@naver.com>
2023-10-11 19:30:53 +0900

    스티커모으기 정답

--- 프로그래머스/스티커 모으기(2).py
+++ 프로그래머스/스티커 모으기(2).py
@@ -1,19 +1,14 @@
 def solution(sticker):
     answer = 0
-    if len(sticker) == 1:
-        return sticker
-
-    n = len(sticker)
-    # 1번 선택하는 경우 -> 1..n-1번 배열에 대한 DP
-    dp1 = [0] * sticker[:-1]
-    for i in range(2, n):
-        dp1[i] = max(dp1[i-1], dp1[i-2] + dp1[i])
+    if len(sticker) <= 3:
+        return max(sticker)
     
-    # 2번 선택하는 경우 -> 2...n번 배열에 대한 DP
-    dp2 = [0] + sticker[1:]
-    for i in range(2, n):
-        dp2[i] = max(dp2[i-1], dp2[i-2] + dp2[i])
-        
-    answer = max(dp1[-1], dp2[-1])
-    return answer
-print(solution([14, 6, 5, 11, 3, 9, 2, 10]))
+    n = len(sticker)
+    l1 = [0] + sticker[: -1]
+    l2 = [0] + sticker[1:]
+    for i in range(2,n):
+        l1[i] = max(l1[i] + l1[i-2], l1[i-1])
+    for i in range(2,n):
+        l2[i] = max(l2[i] + l2[i-2], l2[i-1])
+
+    return max(l1[-1],l2[-1])
